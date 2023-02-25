472c74e2b76a24e3c7a68a0db2501336452511ce
Jithin A P <jithinap009@gmail.com>
2023-02-25 13:40:28 +0530

    Configured mongoose

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,11 +4,13 @@ import morgan from 'morgan'
 import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
 import clientRoutes from './routes/client'
 import generalRoutes from './routes/general'
 import managementRoutes from './routes/management'
 import salesRoutes from './routes/sales'
 
+const PORT = process.env.PORT ?? 5000
 
 // Configurations
 dotenv.config()
@@ -26,5 +28,12 @@ app.use('/client', clientRoutes)
 app.use('/general', generalRoutes)
 app.use('/management', managementRoutes)
 app.use('/sales', salesRoutes)
+
+// Mongoose Setup
+mongoose.connect(process.env.MONGO_URL!).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`)
+  })
+}).catch((error) => console.log(`${error}`))
 
 
